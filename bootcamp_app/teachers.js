d2844b102c6946e1ccaf50451b1bbeebaeb1236c
Daryan Zycki <da.zycki@gmail.com>
2022-04-21 01:56:26 +0000

    Add Node Postgres to query db using js.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+const process = require('process');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(result => {
+    console.log(`${result.cohort}: ${result.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
