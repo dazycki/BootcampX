a149b54823831bdd94e1da5647f6086b040fc76b
Daryan Zycki <da.zycki@gmail.com>
2022-04-21 03:15:39 +0000

    Add query string and pass to pool.query.

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,20 +8,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2] || 'JUL02';
-const values = [`%${cohortName}%`];
-
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
+const queryString = `
+  SELECT teachers.name as teacher, cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
   WHERE cohorts.name = $1
-  ORDER BY teacher;
-`)
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teachers.name;
+`
+
+const cohortName = `${process.argv[2] || 'JUL02'}`;
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
